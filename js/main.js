6fcd4fff2ff6487a0c18673bc0873cee4a890257
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-02-21 14:20:11 -0300

    añadido js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@
 let apellido = prompt("ingresa tu apellido");
 let salida = nombre + " " +  apellido + " " + "welcome to this page";
 alert (salida);*/
-<script src="https://kit.fontawesome.com/afaf9d511e.js" crossorigin="anonymous"></script>
+
+saludo = "Bienvenido a Absent Void"
+alert(saludo)
