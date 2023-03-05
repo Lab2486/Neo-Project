2ddb917dfaf36457c0a2992d2a0bc9568e22b4cb
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-03-05 12:59:13 -0300

    creo que ya estaria la primer pre entrega

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ let nombre = prompt ("Ingresa tu nombre")
 function saludo () {
     if (nombre == ""){
         alert ("No ingresaste tu nombre")
-        let nombre = prompt ("Vuelve a ingresar tu nombre")
-        alert ("Bienvenido" + " " + nombre)
-    } else{
-        alert ("Bienvenido" + " " + nombre)
+        for (let nombre = 0; nombre <= 100; nombre ++){
+            let nombre = prompt ("Solo tenes que escribir tu nombre o este mensaje va a aparecer para siempre")
+            if (nombre != ""){
+                alert ("Bienvenido" + " " + nombre + " "+ "tan dificil era?")
+                break
+            }
+        }
+    }else{
+        alert ("Bienvenido" + " " + nombre + " " +"que pro")
     }
-}
+    }
 
 saludo()
 
