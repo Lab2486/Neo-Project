b2552f50c0778ffebd1bed167d45a7c93b7b7941
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-03-09 12:32:06 -0300

    Prueba de function y otras cosas de JS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
-let nombre = prompt ("Ingresa tu nombre")
+// let nombre = prompt ("Ingresa tu nombre")
 
 
-function saludo () {
-    if (nombre == ""){
-        alert ("No ingresaste tu nombre")
-        for (let nombre = 0; nombre <= 100; nombre ++){
-            let nombre = prompt ("Solo tenes que escribir tu nombre o este mensaje va a aparecer para siempre")
-            if (nombre != ""){
-                alert ("Bienvenido" + " " + nombre + " "+ "tan dificil era?")
-                break
-            }
-        }
-    }else{
-        alert ("Bienvenido" + " " + nombre + " " +"que pro")
+// function saludo () {
+//     if (nombre == ""){
+//         alert ("No ingresaste tu nombre")
+//         for (let nombre = 0; nombre <= 100; nombre ++){
+//             let nombre = prompt ("Solo tenes que escribir tu nombre o este mensaje va a aparecer para siempre")
+//             if (nombre != ""){
+//                 alert ("Bienvenido" + " " + nombre + " "+ "tan dificil era?")
+//                 break
+//             }
+//         }
+//     }else{
+//         alert ("Bienvenido" + " " + nombre + " " +"que pro")
+//     }
+//     }
+
+// saludo()
+
+
+
+const tracks = [agregarTracks];
+function agregarTracks (nombre, ep, artista, sello, precio){
+    tracks.push (new Track (nombre, ep, artista, sello, precio))
+}
+class Track {
+    constructor (nombre, ep, artista, sello, precio){
+        this.nombre = nombre;
+        this.ep = ep;
+        this.artista = artista;
+        this.sello = sello;
+        this.precio = parseFloat.precio;
     }
-    }
+}
 
-saludo()
+agregarTracks("Little More", "EPJOKER", "JOKR", "JOKERBLACKLABEL", "20");
 
-
-
-
-
-
+console.log (tracks)
 
 
 
