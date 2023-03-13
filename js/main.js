782d8ada1d2f447cc1f588e20c948c43a3f971b5
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-03-13 12:19:49 -0300

    filtro de generos ya realizado falta hace un array de carrito para que se sumen ahi los tracks comprados

--- js/main.js
+++ js/main.js
@@ -20,19 +20,18 @@
 
 
 
-
 class Tracks {
     constructor (trackName, epName,artist, price, id){
         this.trackName = trackName;
         this.epName = epName;
-        this.artis = artist;
+        this.artist = artist;
         this.price = price
         this.id = id
     }
 }
 
-const track1 = new Tracks ("Al Final Del Tunel", "Al final del tuner (EP)", "Kold", 20, "m01");
-const track2 = new Tracks ("Dark Sequense", "Al final del tuner (EP)", "Kold", 20, "m02");
+const track1 = new Tracks ("Al Final Del Tunel", "Al final del tunel (EP)", "Kold", 20, "m01");
+const track2 = new Tracks ("Dark Sequense", "Al final del tunel (EP)", "Kold", 20, "m02");
 const track3 = new Tracks ("All Night Long", "Head Hunter (EP)", "Jokr", 20, "m03");
 const track4 = new Tracks ("Head Hunter", "Head Hunter (EP)", "Jokr", 20, "m04");
 const track5 = new Tracks ("Ninja", "Head Hunter (EP)", "Jokr", 20, "m05")
@@ -46,30 +45,43 @@ const track10 = new Tracks ("Check This Out", "Making The Groove (EP)", "Anvii",
 
 const minimal = [track1, track2, track3, track4, track5]
 const house = [track6, track7, track8, track9, track10]
+const carrito = []
 
 
 let filtro = prompt ("Ingresa: \n 1) Para Minimal \n 2) Para house")
 
 
-
-
 function saludo () {
+    
     if (filtro == "1"){
         for (const track of minimal) {
-            console.log(`${track.trackName} ${track.epName} ${track.artis} $${track.price} ID:${track.id}`)
+            console.log(`${track.trackName} ${track.epName} ${track.artist} $${track.price} ID:${track.id}`)
         }
-        prompt ("ingresa el ID del track que quieres comprar")
-
-        
-    }else if (filtro == "2"){
+    }
+    
+    if (filtro == "2"){
         for (const track of house) {
-            console.log(`${track.trackName} ${track.epName} ${track.artis} $${track.price} ID:${track.id}`)
-        }
-        
+            console.log(`${track.trackName} ${track.epName} ${track.artist} $${track.price} ID:${track.id}`)
+        }    
     }
     }
+
+
 
 saludo()
+let compra = prompt ("ingresa el ID del track que desear comprar")
+
+function buy () {
+    if (compra == "m01"){
+        console.log (`felicidades compraste  ${track1.trackName} by ${track1.artist}`)
+    }
+}
+buy ()
+
+
+
+
+
 
 
 
