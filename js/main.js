379d0dac64a69aa412d5107ecaa16261cca14f15
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-03-30 13:02:29 -0300

    intento de usar dom putamadre

--- js/main.js
+++ js/main.js
@@ -1,94 +1,189 @@
-const tracks = [ 
-{nombre: 'Al Final Del Tunel', artista:'Kold',genero: 'Minimal', codigo: "m01"},
-{nombre: 'Dark Sequence', artista:'Kold',genero: 'Minimal', codigo: "m02"},
-{nombre: 'All Night Long', artista:'Jokr',genero: 'Minimal', codigo: "m03"},
-{nombre: 'Head Hunter', artista:'Jokr',genero: 'Minimal', codigo: "m04"},
-{nombre: 'Ninja', artista:'Jokr',genero: 'Minimal', codigo: "m05"},
-{nombre: 'White Lights', artista:'Fenoma',genero: 'House', codigo: "h01"},
-{nombre: 'Feel It', artista:'Fenoma',genero: 'House', codigo: "h02"},
-{nombre: 'Making The Groove', artista:'Anvii',genero: 'House', codigo: "h03"},
-{nombre: 'Rojaiju', artista:'Anvii',genero: 'House', codigo: "h04"},
-{nombre: 'Check This out', artista:'Anvii',genero: 'House', codigo: "h05"}]; //Todos los Tacks
+const contenedorProductos = document.getElementById("contenedorProductos");
+const catalogoDeTracks = [
+  {
+    nombre: "Al Final Del Tunel",
+    artista: "Kold",
+    genero: "Minimal",
+    id: "m01",
+    imagen: "../media/al-final-del-tunel-kold.jpg",
+    sound: "../media/KOLD - Al Final Del Túnel (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "Minimal",
+      id: "minimal",
+    },
+  },
+  {
+    nombre: "Dark Sequence",
+    artista: "Kold",
+    genero: "Minimal",
+    id: "m02",
+    imagen: "../media/al-final-del-tunel-kold.jpg",
+    sound: "../media/KOLD - Dark Sequence (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "Minimal",
+      id: "minimal",
+    },
+  },
+  {
+    nombre: "Ninja",
+    artista: "JØKR",
+    genero: "Minimal",
+    id: "m03",
+    imagen: "../media/jokr-head-hunter-EP.jpg",
+    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    precio: 200,
+    categoria: {
+      nombre: "Minimal",
+      id: "minimal",
+    },
+  },
+  {
+    nombre: "All Night Long",
+    artista: "JØKR",
+    genero: "Minimal",
+    id: "m04",
+    imagen: "../media/jokr-head-hunter-EP.jpg",
+    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    precio: 200,
+    categoria: {
+      nombre: "Minimal",
+      id: "minimal",
+    },
+  },
+  {
+    nombre: "Head Hunter",
+    artista: "JØKR",
+    genero: "Minimal",
+    id: "m05",
+    imagen: "../media/jokr-head-hunter-EP.jpg",
+    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    precio: 200,
+    categoria: {
+      nombre: "Minimal",
+      id: "minimal",
+    },
+  },
+  {
+    nombre: "Making The Goove",
+    artista: "Anvii",
+    genero: "Minimal",
+    id: "h01",
+    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
+    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "House",
+      id: "hose",
+    },
+  },
+  {
+    nombre: "Rojaiju",
+    artista: "Anvii",
+    genero: "Minimal",
+    id: "h02",
+    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
+    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "House",
+      id: "house",
+    },
+  },
+  {
+    nombre: "Check This Out",
+    artista: "Anvii",
+    genero: "Minimal",
+    id: "h03",
+    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
+    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "House",
+      id: "house",
+    },
+  },
+  {
+    nombre: "White Lights",
+    artista: "Fenoma",
+    genero: "Minimal",
+    id: "h04",
+    imagen: "../media/Fenoma-White-Lights-EP.jpg",
+    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "House",
+      id: "house",
+    },
+  },
+  {
+    nombre: "Feel It",
+    artista: "Fenoma",
+    genero: "Minimal",
+    id: "h05",
+    imagen: "../media/Fenoma-White-Lights-EP.jpg",
+    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    precio: 200,
+    categoria: {
+      nombre: "House",
+      id: "house",
+    },
+  },
+];
 
-const tracksDeMinimal = tracks.filter ((track) => track.genero == 'Minimal'); //Tacks de Minimal
-const tracksDeHouse = tracks.filter ((track) => track.genero == 'House'); //Tracks de House
+catalogoDeTracks.forEach((product) => {
+  let content = document.createElement("div");
+  content.className = "col-md-4 col-sm-6 ep ";
+  content.innerHTML = `
+    <img src="${product.imagen}" alt="${product.nombre}">
+    <h3 class="ep__title fs-4">${product.nombre}</h3>
+    <p class="ep__artist fs-3">${product.artista}</p>
+    <audio src="${product.sound}" controls></audio>
+    `;
 
-// let filtro = prompt ('Ingresa: \n 1) Para Minimal \n 2) Para house  \n 3) Para ver todos los tracks disponibles')
+  contenedorProductos.append(content);
 
-function saludo () {
-    if (filtro == "1"){
-            for (const track of tracksDeMinimal) {
-                console.log(`${track.nombre} / ${track.artista} / ${track.genero} / Codigo: ${track.codigo}`)
-            }
-    }
+  let comprar = document.createElement("button");
+  comprar.innerText = "BUY";
+  comprar.className = "btn btn-outline-light boton";
 
-    if (filtro == "2"){
-            for (const track of tracksDeHouse) {
-                console.log(`${track.nombre} / ${track.artista} / ${track.genero} / Codigo: ${track.codigo}`)
-                }
-    }
+  content.append(comprar);
+});
 
-    if (filtro == "3"){
-        for (const track of tracks) {
-            console.log(`${track.nombre} / ${track.artista} / ${track.genero} / Codigo: ${track.codigo}`)
-            }
+// const contenedorProductos = document.getElementById("product-list");
+
+// function cargarTracks() {
+//   catalogoDeTracks.forEach((track) => {
+//     const div = document.createElement("<div></div>");
+//     div.classList.add("col-md-4 col-sm-6 ep product-item minimal");
+//     div.innerHTML = `
+//       <img class="product__img" src="${track.imagen}" alt="${track.nombre}" />
+//       <div class="product__details">
+//       <h3 class="product__title">${track.nombre}</h3>
+//       <h4 class="product__subtitle">${track.artista}</h4>
+//       <p class="product__price">${track.precio}</p>
+//       <audio class="product__audio" src="${track.sound}" controls></audio>
+//       <button class="product__buy" id="${track.id}">Buy</button>
+//       </div>
+//       `;
+//     contenedorTracks.append(div);
+//   });
+// }
+
+// cargarTracks();
+
+{
+  /* <div class="col-md-4 col-sm-6 ep product-item minimal" category="minimal">
+              <div class="ep__columna pb-4">
+                  <p class="ep__artist fs-3">Jokr</p>
+                  <img  class="ep__img" src="./img/jokr-head-hunter-EP.jpg" alt="·">
+                  <h3 class="ep__title fs-6">Head Hunter (EP)</h3>
+                  <p class="ep__subtitle fs-4">Ninja M05</p>
+                  <p class="ep__subtitle">(Original Mix)</p>
+                  <audio class="ep__audio" src="./img/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3" controls controlslist="nodownload"></audio>
+                  <button type="button" class="btn btn-outline-light boton">Add to cart</button>
+               </div>
+
+            </div> */
 }
-
-}
-
-// saludo ()
-
-let carrito = [];
-
-function comprar () {
-        let buy = prompt ('Ingresa el codigo del track que deseas comprar')
-        console.log (tracks.find((track) =>  buy == track.codigo))
-        carrito.push (buy);
-        console.log (carrito)   
-    };
-
-// comprar ()
-
-function comprarMas () {
-    let continuarCompra = prompt ('Deseas comprar otro track? \n 1) SI \n 2) NO')
-    if (continuarCompra == "1"){
-        let buy = prompt ('Ingresa el codigo del track que deseas comprar')
-        console.log (tracks.find((track) =>  buy == track.codigo))
-        carrito.push (buy);
-        console.log (carrito) 
-    }
-    if (continuarCompra == "2"){
-        alert ('Gracias por su compra')
-    }
-}
-
-// comprarMas ()
-
-const btnAll = document.getElementById ("btnAll"),
-btnHouse = document.getElementById ("btnHouse"),
-btnMinimal = document.getElementById ("btnMinimal"),
-btnPsytrance = document.getElementById ("btnPsytrance"),
-muestra = document.getElementById ("product-list")
-
-btnHouse.onclick = ()=>{
-    
-}
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
