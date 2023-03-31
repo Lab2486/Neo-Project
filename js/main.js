17f43d41c6b87beb09b5bfd80bc36355c4fc2982
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-03-31 14:11:30 -0300

    avance en el project

--- js/main.js
+++ js/main.js
@@ -1,121 +1,87 @@
 const contenedorProductos = document.getElementById("contenedorProductos");
+const contenedorCarrito = document.getElementById("carritoContenedor");
+const btnVaciar = document.getElementById("vaciarCarrito");
 const catalogoDeTracks = [
   {
     nombre: "Al Final Del Tunel",
     artista: "Kold",
     genero: "Minimal",
     id: "m01",
-    imagen: "../media/al-final-del-tunel-kold.jpg",
-    sound: "../media/KOLD - Al Final Del Túnel (Original Mix).mp3",
+    imagen: "./media/al-final-del-tunel-kold.jpg",
+    sound: "./media/KOLD - Al Final Del Túnel (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "Minimal",
-      id: "minimal",
-    },
   },
   {
     nombre: "Dark Sequence",
     artista: "Kold",
     genero: "Minimal",
     id: "m02",
-    imagen: "../media/al-final-del-tunel-kold.jpg",
-    sound: "../media/KOLD - Dark Sequence (Original Mix).mp3",
+    imagen: "./media/al-final-del-tunel-kold.jpg",
+    sound: "./media/KOLD - Dark Sequence (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "Minimal",
-      id: "minimal",
-    },
   },
   {
     nombre: "Ninja",
     artista: "JØKR",
     genero: "Minimal",
     id: "m03",
-    imagen: "../media/jokr-head-hunter-EP.jpg",
-    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    imagen: "./media/jokr-head-hunter-EP.jpg",
+    sound: "./media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
     precio: 200,
-    categoria: {
-      nombre: "Minimal",
-      id: "minimal",
-    },
   },
   {
     nombre: "All Night Long",
     artista: "JØKR",
     genero: "Minimal",
     id: "m04",
-    imagen: "../media/jokr-head-hunter-EP.jpg",
-    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    imagen: "./media/jokr-head-hunter-EP.jpg",
+    sound: "./media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
     precio: 200,
-    categoria: {
-      nombre: "Minimal",
-      id: "minimal",
-    },
   },
   {
     nombre: "Head Hunter",
     artista: "JØKR",
     genero: "Minimal",
     id: "m05",
-    imagen: "../media/jokr-head-hunter-EP.jpg",
-    sound: "../media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
+    imagen: "./media/jokr-head-hunter-EP.jpg",
+    sound: "./media/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3",
     precio: 200,
-    categoria: {
-      nombre: "Minimal",
-      id: "minimal",
-    },
   },
   {
     nombre: "Making The Goove",
     artista: "Anvii",
     genero: "Minimal",
     id: "h01",
-    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
-    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    imagen: "./media//Anvii-Making-the-groove-EP.jpg",
+    sound: "./media/Anvii - Making The Groove (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "House",
-      id: "hose",
-    },
   },
   {
     nombre: "Rojaiju",
     artista: "Anvii",
     genero: "Minimal",
     id: "h02",
-    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
-    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    imagen: "./media//Anvii-Making-the-groove-EP.jpg",
+    sound: "./media/Anvii - Making The Groove (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "House",
-      id: "house",
-    },
   },
   {
     nombre: "Check This Out",
     artista: "Anvii",
     genero: "Minimal",
     id: "h03",
-    imagen: "../media//Anvii-Making-the-groove-EP.jpg",
-    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    imagen: "./media//Anvii-Making-the-groove-EP.jpg",
+    sound: "./media/Anvii - Making The Groove (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "House",
-      id: "house",
-    },
   },
   {
     nombre: "White Lights",
     artista: "Fenoma",
     genero: "Minimal",
     id: "h04",
-    imagen: "../media/Fenoma-White-Lights-EP.jpg",
-    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    imagen: "./media/Fenoma-White-Lights-EP.jpg",
+    sound: "./media/Anvii - Making The Groove (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "House",
-      id: "house",
-    },
   },
   {
     nombre: "Feel It",
@@ -123,67 +89,62 @@ const catalogoDeTracks = [
     genero: "Minimal",
     id: "h05",
     imagen: "../media/Fenoma-White-Lights-EP.jpg",
-    sound: "../media/Anvii - Making The Groove (Original Mix).mp3",
+    sound: "./media/Anvii - Making The Groove (Original Mix).mp3",
     precio: 200,
-    categoria: {
-      nombre: "House",
-      id: "house",
-    },
   },
 ];
 
+let carrito = [];
+
+btnVaciar.addEventListener("click", () => {
+  carrito.length = 0;
+  actualizarCarrito();
+});
+
 catalogoDeTracks.forEach((product) => {
   let content = document.createElement("div");
-  content.className = "col-md-4 col-sm-6 ep ";
+  content.className = "ep ep__columna ";
   content.innerHTML = `
-    <img src="${product.imagen}" alt="${product.nombre}">
+    <img class="ep__img" src="${product.imagen}" alt="${product.nombre}">
     <h3 class="ep__title fs-4">${product.nombre}</h3>
     <p class="ep__artist fs-3">${product.artista}</p>
-    <audio src="${product.sound}" controls></audio>
+    <audio class="ep__audio" src="${product.sound}" controls></audio>
+    <button id="agregar${product.id}" class="btn btn-outline-light boton boton__agregar">BUY</button>
     `;
 
   contenedorProductos.append(content);
 
-  let comprar = document.createElement("button");
-  comprar.innerText = "BUY";
-  comprar.className = "btn btn-outline-light boton";
-
-  content.append(comprar);
+  const boton = document.getElementById(`agregar${product.id}`);
+  boton.addEventListener("click", () => {
+    addToCart(product.id);
+  });
 });
 
-// const contenedorProductos = document.getElementById("product-list");
+const addToCart = (prodId) => {
+  const item = catalogoDeTracks.find((prod) => prod.id === prodId);
+  carrito.push(item);
+  actualizarCarrito();
+  console.log(carrito);
+};
 
-// function cargarTracks() {
-//   catalogoDeTracks.forEach((track) => {
-//     const div = document.createElement("<div></div>");
-//     div.classList.add("col-md-4 col-sm-6 ep product-item minimal");
-//     div.innerHTML = `
-//       <img class="product__img" src="${track.imagen}" alt="${track.nombre}" />
-//       <div class="product__details">
-//       <h3 class="product__title">${track.nombre}</h3>
-//       <h4 class="product__subtitle">${track.artista}</h4>
-//       <p class="product__price">${track.precio}</p>
-//       <audio class="product__audio" src="${track.sound}" controls></audio>
-//       <button class="product__buy" id="${track.id}">Buy</button>
-//       </div>
-//       `;
-//     contenedorTracks.append(div);
-//   });
-// }
+const eliminarDelCarrito = (prodId) => {
+  const item = carrito.find((prod) => prod.id === prodId);
+  const indice = carrito.indexOf(item);
+  carrito.splice(indice, 1);
+  actualizarCarrito();
+};
 
-// cargarTracks();
-
-{
-  /* <div class="col-md-4 col-sm-6 ep product-item minimal" category="minimal">
-              <div class="ep__columna pb-4">
-                  <p class="ep__artist fs-3">Jokr</p>
-                  <img  class="ep__img" src="./img/jokr-head-hunter-EP.jpg" alt="·">
-                  <h3 class="ep__title fs-6">Head Hunter (EP)</h3>
-                  <p class="ep__subtitle fs-4">Ninja M05</p>
-                  <p class="ep__subtitle">(Original Mix)</p>
-                  <audio class="ep__audio" src="./img/JØKR - Ninja (Original Mix) Bandcamp Exclusive.mp3" controls controlslist="nodownload"></audio>
-                  <button type="button" class="btn btn-outline-light boton">Add to cart</button>
-               </div>
-
-            </div> */
-}
+const actualizarCarrito = () => {
+  contenedorCarrito.innerHTML = "";
+  carrito.forEach((product) => {
+    const div = document.createElement(`div`);
+    div.className = "producto__en__carrito";
+    div.innerHTML = `
+    <p>${product.nombre}</p>
+    <p>Precio:${product.precio}</p>
+    <button onClick = "eliminarDelCarrito('${product.id}')" class="btn__eliminar"> <i class="bi bi-trash3-fill"></i></button>
+    `;
+    contenedorCarrito.appendChild(div);
+  });
+  contadorCarrito.innerText = carrito.length;
+};
